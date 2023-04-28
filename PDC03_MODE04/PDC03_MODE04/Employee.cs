18dde8802f70b067d8da870302d25c6a53fc5d01
ten <fernandez.kirstenpearl@auf.edu.ph>
2023-04-28 10:51:39 +0800

    first commit

---
+++ PDC03_MODE04/PDC03_MODE04/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDC03_MODE04
+{
+    public class Employee
+    {
+
+        public string DisplayName { get; set; }
+
+        public string Position { get; set; }
+    }
+}
---
+++ PDC03_MODE04/PDC03_MODE04/activity2.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC03_MODE04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    
+    public partial class activity2 : ContentPage
+    {
+        ObservableCollection<Employee> employees = new ObservableCollection<Employee>();
+        public ObservableCollection<Employee> Employees { get { return employees;  } }
+        public activity2()
+        {
+            InitializeComponent();
+
+            EmployeeView.ItemsSource= employees;
+            employees.Add(new Employee { DisplayName = "Juan Dela Cruz", Position = "President" });
+            employees.Add(new Employee { DisplayName = "John Doe", Position = "Vice President" });
+            employees.Add(new Employee { DisplayName = "Peter Cruz", Position = "Director" });
+            employees.Add(new Employee { DisplayName = "Mark Dela Cruz", Position = "Supervisor" });
+
+        }
+
+        private void Add_item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            {
+
+            }
+        }
+         
+        private void Edit_item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            {
+
+            }
+        }
+
+        private void Delete_item(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            if (menuItem != null)
+            {
+
+            }
+        }
+    }
+}
